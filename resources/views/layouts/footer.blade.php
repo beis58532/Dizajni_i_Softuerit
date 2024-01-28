0f8fcfada452e698624e8b5d9ec08e9ae7721a3c
Beis Lushta <bl58532@ubt-uni.net>
2024-01-28 19:06:58 +0100

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer">
 <!-- Log on to codeastro.com for more projects! -->
-     ©  {{date('Y')}} Simple Attendance Management System <span class="d-none d-sm-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Ali Aqa Atayee</span>.
+     ©  {{date('Y')}} Time Attendance Management System <span class="d-none d-sm-inline-block"> - UBT <i class="mdi mdi-heart text-danger"></i> by BEIS</span>.
 </footer>
