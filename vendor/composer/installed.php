811fc65bd98601284d4276d01975f22c246f0a7d
Beis Lushta <bl58532@ubt-uni.net>
2024-01-28 18:50:49 +0100

    db
    databaza

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1bac46f0db698bc532545fc1c455882c440cd647',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -404,9 +404,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1bac46f0db698bc532545fc1c455882c440cd647',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
